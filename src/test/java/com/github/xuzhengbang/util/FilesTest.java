88c9f63b7ea4419001d23dfb4dc2015e158de6c5
xuzhengbang <929351993@qq.com>
2021-08-17 18:09:21 +0800

    Update FilesTest.java

--- src/test/java/com/github/xuzhengbang/util/FilesTest.java
+++ src/test/java/com/github/xuzhengbang/util/FilesTest.java
@@ -4,31 +4,100 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
 
 public class FilesTest {
 
-	public static void main(String[] args) throws IOException {
-//		Path path = Paths.get("/a/b/c/d");
-//		Files.exists(path, options);// java.nio.file.LinkOption.NOFOLLOW_LINKS
+	private Path BASE_PATH = Paths.get("D:\\usr\\tmp\\2021\\08\\files");
 
-		Path existingFilePath = Paths.get("D:\\usr\\tmp\\2021\\08\\link", "text.txt");
-		Path symLinkPath = Paths.get("D:\\usr\\tmp\\2021\\08\\link", "1_link");
-//		Files.createSymbolicLink(symLinkPath, existingFilePath);
-		
-		// TODO 硬链接 软连接
-		boolean symbolicLink = Files.isSymbolicLink(symLinkPath);
-		System.out.println(symbolicLink);
-		
-		
-		Path path1 = Paths.get("/1/2");
-		Path path2 = Paths.get("/1/2");
-		Path subpath = path2.subpath(0, path2.getNameCount());
-		Path resolve = path1.resolve("3").resolve(subpath).resolve("4.txt");
-		System.out.println(resolve);
-		
-		
-		Path path = Paths.get(path1.toString(), "3", path2.toString(), "4.txt");
-		System.out.println(path);
+	/**
+	 * 硬链接
+	 */
+	@Test
+	public void hardLinkTest() {
+		try {
+			Path createFile = Files.createFile(BASE_PATH.resolve("Hard_Link.txt"));
+			Files.write(createFile, "Hard Link".getBytes());
+
+			Path createLink1Path = BASE_PATH.resolve("Hard_Link_1.txt");
+			Files.createLink(createLink1Path, createFile);
+			Path createLink2Path = BASE_PATH.resolve("Hard_Link_2.txt");
+			Files.createLink(createLink2Path, createLink1Path);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// https://www.cnblogs.com/Naylor/p/7597869.html
+	@Test
+	public void symbolicLinkTest() {
+		try {
+			Path createDirectories = Files.createDirectories(BASE_PATH.resolve("Symbolic_Link_Target"));
+			ConsoleUtils.println("Files.createDirectories(BASE_PATH.resolve(\"Symbolic_Link\"))", createDirectories);
+
+			Path createFile = Files.createFile(createDirectories.resolve("Symbolic_Link_Target.txt"));
+			Files.write(createFile, "Symbolic Link Target".getBytes());
+			Path createDirectory = Files.createDirectory(createDirectories.resolve("Symbolic_Link_In"));
+
+			// 需以管理员运行
+			Path symbolicLink1Path = BASE_PATH.resolve("Symbolic_Link_1");
+			Files.createSymbolicLink(symbolicLink1Path, createDirectories);
+			// CMD mklink /d Symbolic_Link1 Symbolic_Link
+
+			ConsoleUtils.println("Files.isSymbolicLink(symbolicLink1Path)", Files.isSymbolicLink(symbolicLink1Path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void walk() {
+		try {
+			List<Path> collect = Files.walk(BASE_PATH) //
+					.filter(item -> item.compareTo(BASE_PATH) != 0) //
+					.sorted(new Comparator<Path>() {
+						@Override
+						public int compare(Path o1, Path o2) {
+							return Integer.compare(o1.getNameCount(), o2.getNameCount()) * -1;// 路径最长倒叙
+						}
+					}).collect(Collectors.toList());
+			// 先删除最底层的文件/文件夹
+			for (Path path : collect) {
+				System.out.println(path);
+				Files.delete(path);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void delete() {
+		try {
+			Path deleteIfExistsPath = Files.createFile(BASE_PATH.resolve("deleteIfExists.txt"));
+			Files.write(deleteIfExistsPath, "deleteIfExists".getBytes());
+			ConsoleUtils.println("Files.deleteIfExists(deleteIfExistsPath)", Files.deleteIfExists(deleteIfExistsPath));
+
+			Path deletePath = Files.createFile(BASE_PATH.resolve("delete.txt"));
+			Files.write(deletePath, "delete".getBytes());
+			Files.delete(deletePath);
+			ConsoleUtils.println("Files.delete(deletePath)", "OK");
+
+			ConsoleUtils.println("Files.deleteIfExists(deletePath)", Files.deleteIfExists(deletePath));
+			try {
+				Files.delete(deleteIfExistsPath);
+			} catch (Exception e) {
+				ConsoleUtils.println("Files.delete(deleteIfExistsPath)",
+						e.getClass().getName() + " : " + e.getMessage());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
